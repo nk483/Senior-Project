5cd0a6b7197671f81ef91d0f37d12456266daba9
Ubuntu <ubuntu@ip-172-31-3-212.us-east-2.compute.internal>
2023-04-28 21:38:03 +0000

    Removed Hockey due to lack of usable obs (6809), and added 'n=' to M and R over time by sport plot

--- descriptive_stats.py
+++ descriptive_stats.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import math
 #This file creates a bar chart of average M and R per match by sportsbook
 # and plots average M and R over the course of a game for each sport
 
@@ -16,52 +17,36 @@ ax.set_xlabel('Sportsbook')
 ax.set_ylabel('Average M and R')
 ax.tick_params(axis='x', which='major', labelsize=8)
 ax.set_yticks(np.arange(0, .25, .025))
+ax.set_title("Average M and UR by Sportsbook")
 newticks = mean_df['Sportsbook'].values
 newticks[0] = 'Barstool'
 newticks[-1] = 'Hill'
 newticks[-5] = 'PointsBet'
 ax.set_xticklabels(newticks)
-plt.savefig('figures/Average Mov and UR by Sportsbook')
 
-# result = grouped.apply(calc_mov_ur)
-# result = result.apply(lambda x: pd.Series(x))
-# grouped = result.reset_index().groupby(['Sportsbook'])
-# avg_mov = grouped['Movement'].mean().reset_index()
-# avg_mov = avg_mov.rename(columns = {"Movement" : "Average Movement"})
-# avg_ur = grouped['Uncertainty Reduction'].mean()
-# combined = avg_mov.assign(Average_UR=avg_ur.values)
-# # plot bar chart
-# fig,ax = plt.subplots(figsize=(8,9))
-# ax = combined.plot(x='Sportsbook', y=['Average Movement', 'Average_UR'], kind='bar', ax=ax)
-# ax.set_xlabel('Sportsbook')
-# ax.set_ylabel('Average M and R')
-# ax.tick_params(axis='x', which='major', labelsize=8)
-# newticks = combined['Sportsbook'].values
-# newticks[0] = 'Barstool'
-# newticks[-1] = 'Hill'
-# newticks[-5] = 'PointsBet'
-# ax.set_xticklabels(newticks)
-# plt.savefig('Average Mov and UR by Sportsbook')
+plt.savefig('figures/Average Mov and UR by Sportsbook')
 
 
 #Movement and UR over time plot
 game_lengths = df.groupby(['Sport', 'id', 'Sportsbook'])['Minute Mark'].max()
+sport_obs = df.groupby('Sport').size()
 avg_lengths = game_lengths.groupby('Sport').mean()
 chunk_sizes = avg_lengths / 24
 grouped = df.groupby(['Sport', 'Chunk'])[['Movement', 'Uncertainty Reduction']].mean().reset_index()
 grouped['Size'] = df.groupby(['Sport', 'Chunk']).size().values
 # plot the results
-fig, axes = plt.subplots(nrows=len(grouped['Sport'].unique()) // 2, ncols=2, figsize=(8, 12))
+fig, axes = plt.subplots(nrows=math.ceil(len(grouped['Sport'].unique()) / 2), ncols=2, figsize=(8, 12))
 
 # iterate over each unique sport and create a separate plot for each one
 i = 0
 for sport, data in grouped.groupby('Sport'):
+    size = sport_obs.loc[sport]
     chunk_size = chunk_sizes.loc[sport]
     j = i % 2
     ax = axes[i // 2][j]
     ax.plot(chunk_size*data['Chunk'], data['Movement'])
     ax.plot(chunk_size*data['Chunk'], data['Uncertainty Reduction'])
-    ax.set_title(sport)
+    ax.set_title(sport + ' (n=' + str(size) + ')')
     ax.set_xlabel('Minute Mark')
     ax.set_ylabel('M and R')
     i += 1
